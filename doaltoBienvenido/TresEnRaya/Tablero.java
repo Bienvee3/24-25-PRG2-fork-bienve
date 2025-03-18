0bd112d561c2cd1eedb707e947327a8204c1fea3
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-03-18 20:06:41 +0100

    edit tablero.java && delete .txt

--- doaltoBienvenido/TresEnRaya/Tablero.java
+++ doaltoBienvenido/TresEnRaya/Tablero.java
@@ -1,5 +1,25 @@
 package doaltoBienvenido.TresEnRaya;
 
 public class Tablero {
+    private char[][] casillas;
 
+    public Tablero() {
+        casillas = new char[3][3];
+
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                casillas[i][j] = '_';
+            }
+        }
+    }
+
+    public mostrar() {
+        cleanScreen();
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                System.out.print(" " + casillas[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
