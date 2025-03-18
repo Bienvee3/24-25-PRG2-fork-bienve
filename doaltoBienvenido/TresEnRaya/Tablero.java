f82b3792ef06c65827af03defd9b303f380142f3
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-03-18 20:25:29 +0100

    tablero modificado

--- doaltoBienvenido/TresEnRaya/Tablero.java
+++ doaltoBienvenido/TresEnRaya/Tablero.java
@@ -13,4 +13,38 @@ public class Tablero {
         }
     }
 
+    public void mostrar() {
+        cleanScreen();
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                System.out.print(" " + casillas[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private void cleanScreen() {
+
+    }
+
+    public boolean hayTresEnRaya() {
+        return !this.hayTresEnRaya('x') || this.hayTresEnRaya('o');
+            }
+        
+    private boolean hayTresEnRaya(char color) {
+                
+    }
+        
+            public boolean estaCompleto(Jugador jugador) {
+        int conteoFichas = 0;
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                if (casillas[i][j] == jugador.colorDeFicha()) {
+                    conteoFichas++;
+                }
+            }
+        }
+        return conteoFichas == 3;
+    }
+
 }
