d44098e6da7f6e39c1d0c955307ea89593c1ab1a
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-03-18 20:32:07 +0100

    delete lineas no utiles

--- doaltoBienvenido/TresEnRaya/Jugador.java
+++ doaltoBienvenido/TresEnRaya/Jugador.java
@@ -3,18 +3,15 @@ package doaltoBienvenido.TresEnRaya;
 public class Jugador {
 
     public void celebrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'celebrar'");
+        
     }
 
     public void ponerFicha(Tablero tablero) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ponerFicha'");
+        
     }
 
     public char colorDeFicha() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'colorDeFicha'");
+        
     }
 
 }
