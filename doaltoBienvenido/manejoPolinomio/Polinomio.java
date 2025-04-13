9bfddc7297843a5097905a34fe9eeaa92d471607
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-13 20:25:16 +0200

    fix: mejora metodo mostrar

--- doaltoBienvenido/manejoPolinomio/Polinomio.java
+++ doaltoBienvenido/manejoPolinomio/Polinomio.java
@@ -1,24 +1,52 @@
 package doaltoBienvenido.manejoPolinomio;
 
 public class Polinomio {
-    private float[] coeficientes;
+    private int[] coeficientes;
 
-    public Polinomio(int gradoMaximo){
-        coeficientes = new float[gradoMaximo + 1];
+    public Polinomio(int gradoMaximo) {
+        coeficientes = new int[gradoMaximo + 1];
     }
 
     public void agregarTerminos(int coeficiente, int exponente) {
         if (exponente >= 0 && exponente < coeficientes.length) {
-            coeficientes[exponente] =+1;
+            coeficientes[exponente] += coeficiente;
         }
     }
 
-    public void mostrarPolinomio(){
-        for (int i = coeficientes.length - 1; i >=0; i++) {
-            if (coeficientes[i]!=0) {
-                System.out.println(coeficientes[i] + " x^ " + i);
+    public void mostrarPolinomio() {
+        boolean primerTermino = true;
+        int coeficiente2;
+    
+        for (int i = coeficientes.length - 1; i >= 0; i--) {
+            coeficiente2 = coeficientes[i];
+    
+            if (coeficiente2 != 0) {
+                int valorAbsoluto = Math.abs(coeficiente2);
+    
+                if (primerTermino) {
+                    if (coeficiente2 < 0) {
+                        System.out.print("-");
+                    }
+                } else {
+                    System.out.print(coeficiente2 < 0 ? " - " : " + ");
+                }
+    
+                if (i == 0) {
+                    System.out.print(valorAbsoluto);
+                } else if (i == 1) {
+                    System.out.print((valorAbsoluto == 1 ? "" : valorAbsoluto) + "x");
+                } else {
+                    System.out.print((valorAbsoluto == 1 ? "" : valorAbsoluto) + "x^" + i);
+                }
+    
+                primerTermino = false;
             }
         }
+    
+        if (primerTermino) {
+            System.out.print(0);
+        }
+    
+        System.out.println();
     }
-
 }
