746456721c2b6110d6ef4621ea3d6ce857df5137
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-13 16:23:58 +0200

    feat: clase launcher polinomio finalizada

---
+++ doaltoBienvenido/manejoPolinomio/Launcher.java
@@ -0,0 +1,8 @@
+package doaltoBienvenido.manejoPolinomio;
+
+class Launcher {
+    public static void main(String[] args) {
+        Polinomio polinomio = new Polinomio();
+        polinomio.realizar();
+    }  
+}
