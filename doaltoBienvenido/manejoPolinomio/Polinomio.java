8f232574d6cbd11df539529031121c08dd61ee28
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-13 19:57:46 +0200

    feat: agregarterminos + mostrar

--- doaltoBienvenido/manejoPolinomio/Polinomio.java
+++ doaltoBienvenido/manejoPolinomio/Polinomio.java
@@ -1,12 +1,24 @@
 package doaltoBienvenido.manejoPolinomio;
 
 public class Polinomio {
+    private float[] coeficientes;
 
-    public Polinomio(){
-        
+    public Polinomio(int gradoMaximo){
+        coeficientes = new float[gradoMaximo + 1];
     }
-    public void realizar() {
-        
+
+    public void agregarTerminos(int coeficiente, int exponente) {
+        if (exponente >= 0 && exponente < coeficientes.length) {
+            coeficientes[exponente] =+1;
+        }
+    }
+
+    public void mostrarPolinomio(){
+        for (int i = coeficientes.length - 1; i >=0; i++) {
+            if (coeficientes[i]!=0) {
+                System.out.println(coeficientes[i] + " x^ " + i);
+            }
+        }
     }
 
 }
