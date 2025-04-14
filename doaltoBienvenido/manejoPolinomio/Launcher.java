38ab65e733e7d41759cdb66a407dd4ee4e79fda1
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-14 21:58:12 +0200

    feat: metodo sumar polinomios finalizado + implementado en launcher

--- doaltoBienvenido/manejoPolinomio/Launcher.java
+++ doaltoBienvenido/manejoPolinomio/Launcher.java
@@ -1,11 +1,14 @@
 package doaltoBienvenido.manejoPolinomio;
 
+
+
 class Launcher {
     public static void main(String[] args) {
         Polinomio polinomio = new Polinomio(5);
-        polinomio.agregarTerminos(-2, 2);
-        polinomio.agregarTerminos(-10, 1);
-        polinomio.agregarTerminos(-5, 0);
-        polinomio.mostrarPolinomio();
+
+        polinomio.ingresarPolinomio();
+        polinomio.ingresarPolinomio();
+        System.out.println("La suma es: ");
+        polinomio.sumar();
     }  
 }
--- doaltoBienvenido/manejoPolinomio/Polinomio.java
+++ doaltoBienvenido/manejoPolinomio/Polinomio.java
@@ -1,52 +1,83 @@
 package doaltoBienvenido.manejoPolinomio;
 
+import java.util.Scanner;
+
 public class Polinomio {
-    private int[] coeficientes;
+    private int[][] polinomios; 
+    private int polinomiosIngresados = 0;
+    private int gradoMaximo;
+    private final int POLINOMIOS_MAXIMOS = 10; 
 
     public Polinomio(int gradoMaximo) {
-        coeficientes = new int[gradoMaximo + 1];
+        this.gradoMaximo = gradoMaximo;
+        polinomios = new int[POLINOMIOS_MAXIMOS][gradoMaximo + 1]; 
     }
 
-    public void agregarTerminos(int coeficiente, int exponente) {
-        if (exponente >= 0 && exponente < coeficientes.length) {
-            coeficientes[exponente] += coeficiente;
+    public void ingresarPolinomio() {
+        if (polinomiosIngresados >= POLINOMIOS_MAXIMOS) {
+            System.out.println("No se pueden ingresar más polinomios. ");
+            return;
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de terminos del polinomio: ");
+        int numTerminos = scanner.nextInt();
+
+        for (int i = 0; i < numTerminos; i++) {
+            System.out.println("Ingrese coeficiente y exponente del termino " + (i + 1) + ": ");
+            int coeficienteTermino = scanner.nextInt();
+            int exponenteTermino = scanner.nextInt();
+
+            if (exponenteTermino >= 0 && exponenteTermino <= gradoMaximo) {
+                polinomios[polinomiosIngresados][exponenteTermino] += coeficienteTermino;
+            } else {
+                System.out.println("Exponente fuera de rango (0 a " + gradoMaximo + ")");
+            }
+        }
+
+        polinomiosIngresados++;
     }
 
-    public void mostrarPolinomio() {
+    public void sumar() {
+        int[] resultadoSuma = new int[gradoMaximo + 1];
+
+        for (int i = 0; i < polinomiosIngresados; i++) {
+            for (int j = 0; j <= gradoMaximo; j++) {
+                resultadoSuma[j] += polinomios[i][j];
+            }
+        }
+        mostrarPolinomio(resultadoSuma);
+    }
+
+    private void mostrarPolinomio(int[] coeficientes) {
         boolean primerTermino = true;
-        int coeficiente2;
-    
+
         for (int i = coeficientes.length - 1; i >= 0; i--) {
-            coeficiente2 = coeficientes[i];
-    
-            if (coeficiente2 != 0) {
-                int valorAbsoluto = Math.abs(coeficiente2);
-    
-                if (primerTermino) {
-                    if (coeficiente2 < 0) {
-                        System.out.print("-");
-                    }
-                } else {
-                    System.out.print(coeficiente2 < 0 ? " - " : " + ");
+            int coeficienteMostrado = coeficientes[i];
+            if (coeficienteMostrado != 0) {
+                if (!primerTermino) {
+                    System.out.print(coeficienteMostrado < 0 ? " - " : " + ");
+                } else if (coeficienteMostrado < 0) {
+                    System.out.print("-");
                 }
-    
+
+                int abs = Math.abs(coeficienteMostrado);
                 if (i == 0) {
-                    System.out.print(valorAbsoluto);
+                    System.out.print(abs);
                 } else if (i == 1) {
-                    System.out.print((valorAbsoluto == 1 ? "" : valorAbsoluto) + "x");
+                    System.out.print((abs == 1 ? "" : abs) + "x");
                 } else {
-                    System.out.print((valorAbsoluto == 1 ? "" : valorAbsoluto) + "x^" + i);
+                    System.out.print((abs == 1 ? "" : abs) + "x^" + i);
                 }
-    
+
                 primerTermino = false;
             }
         }
-    
+
         if (primerTermino) {
-            System.out.print(0);
+            System.out.print("0");
         }
-    
+
         System.out.println();
     }
 }
