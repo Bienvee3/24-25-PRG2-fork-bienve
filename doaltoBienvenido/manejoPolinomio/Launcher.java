d36c84a20702511cbd7f54234d049ead180409b6
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-14 18:45:44 +0200

    fix: refactory main

--- doaltoBienvenido/manejoPolinomio/Launcher.java
+++ doaltoBienvenido/manejoPolinomio/Launcher.java
@@ -2,7 +2,10 @@ package doaltoBienvenido.manejoPolinomio;
 
 class Launcher {
     public static void main(String[] args) {
-        Polinomio polinomio = new Polinomio();
-        polinomio.realizar();
+        Polinomio polinomio = new Polinomio(5);
+        polinomio.agregarTerminos(-2, 2);
+        polinomio.agregarTerminos(-10, 1);
+        polinomio.agregarTerminos(-5, 0);
+        polinomio.mostrarPolinomio();
     }  
 }
