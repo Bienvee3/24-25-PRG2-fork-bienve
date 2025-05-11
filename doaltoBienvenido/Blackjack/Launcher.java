d16c9e6be32f0382e96ab50d5a06adae001217d1
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-05-11 16:04:59 +0200

    feat: clase launcher blackjack realizada

---
+++ doaltoBienvenido/Blackjack/Launcher.java
@@ -0,0 +1,8 @@
+package doaltoBienvenido.Blackjack;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Blackjack blackjack = new Blackjack();
+        blackjack.jugar();
+    }
+}
